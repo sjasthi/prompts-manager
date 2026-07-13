aa78859a9cf13f0ca8134737edfe9e4c6a0cda92
hpham24 <hieu.pham@my.metrostate.edu>
2026-07-13 11:14:25 -0500

    add curl timeout fix for Cloudflare image generation

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'prompt_manager');
-
-// Cloudflare AI
-define('CF_ACCOUNT_ID', '1bd11e928d05b2a6aa1f89385aa5fe9d');
-define('CF_API_TOKEN', 'cfut_jFqL07hgNg87plFuCXTeoRM4Afr2L4lXDtGAc4v48cc438bd');

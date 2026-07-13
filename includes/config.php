73fd8bfce712092635d35ffae307723631850632
hpham24 <hieu.pham@my.metrostate.edu>
2026-07-13 18:21:54 -0500

    resolve config.php merge conflict

--- includes/config.php
+++ includes/config.php
@@ -5,5 +5,5 @@ define('DB_PASS', '');
 define('DB_NAME', 'prompt_manager');
 
 // Cloudflare AI
-define('CF_ACCOUNT_ID', '');
-define('CF_API_TOKEN', '');
+define('CF_ACCOUNT_ID', '1bd11e928d05b2a6aa1f89385aa5fe9d');
+define('CF_API_TOKEN', 'cfut_oWvRepVFqJp1UHmg6ptCF3o9vjAWSmt5Er001HyN357b351c');
